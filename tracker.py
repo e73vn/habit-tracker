20cd4f032970be391bc311644ce1404405c2cb8a
e73vn <e.thomas7763@gmail.com>
2026-06-17 00:07:46 +0100

    tracker.py

---
+++ tracker.py
@@ -0,0 +1,21 @@
+habits = []
+
+while True:
+    print("\nHabit Tracker")
+    print("1. Add habit")
+    print("2. View habits")
+    print("3. Quit")
+
+    choice = input("> ")
+
+    if choice == "1":
+        habit = input("New habit: ")
+        habits.append(habit)
+        print("Added!")
+
+    elif choice == "2":
+        for habit in habits:
+            print("-", habit)
+
+    elif choice == "3":
+        break
